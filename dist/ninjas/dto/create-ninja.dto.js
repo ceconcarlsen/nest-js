538fe9e037ff4cdcfef53b420e2a01b5ef953fbd
Gabriel Cecon Carlsen <cecon@207q.fct.unesp.br>
2023-03-20 20:50:55 -0300

    fix: removing dist folder

--- dist/ninjas/dto/create-ninja.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateNinjaDto = void 0;
-class CreateNinjaDto {
-}
-exports.CreateNinjaDto = CreateNinjaDto;
-//# sourceMappingURL=create-ninja.dto.js.map
--- dist/ninjas/dto/update-ninja.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateNinjaDto } from './create-ninja.dto';
-declare const UpdateNinjarDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateNinjaDto>>;
-export declare class UpdateNinjarDto extends UpdateNinjarDto_base {
-}
-export {};
