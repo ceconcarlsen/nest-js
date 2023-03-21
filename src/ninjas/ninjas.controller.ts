88a28a8855bd008167622f20d66b1c98e84dbbad
Gabriel Cecon Carlsen <cecon@207q.fct.unesp.br>
2023-03-20 21:12:10 -0300

    feat: ninja service

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -10,22 +10,25 @@ import {
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjarDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private readonly ninjaService: NinjasService) {}
+
   @Get()
-  getNinjas(@Query('type') type: string) {
-    return [{ type }];
+  getNinjas(@Query('weapon') weapon: 'stars' | 'sword') {
+    return this.ninjaService.getNinjas(weapon);
   }
 
   @Get(':id')
   getOneNinja(@Param('id') id: string) {
-    return { id };
+    return this.ninjaService.getNinja(Number(id));
   }
 
   @Post()
   createNinja(@Body() createNinjaDto: CreateNinjaDto) {
-    return { name: createNinjaDto.name };
+    return this.createNinja(createNinjaDto);
   }
 
   @Put(':id')
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
-@Injectable()
-export class NinjasService {}
+@Injectable() //auto inject when a controller depends on it (cool)
+export class NinjasService {
+  private ninjas = [
+    {
+      id: 0,
+      name: 'ninjaA',
+      weapon: 'stars',
+    },
+    {
+      id: 0,
+      name: 'ninjaB',
+      weapon: 'sword',
+    },
+  ];
+
+  getNinjas(weapon: 'stars' | 'sword') {
+    if (weapon) {
+      return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+    }
+
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+
+    if (!ninja) {
+      throw new Error('ninja not found');
+    }
+
+    return ninja;
+  }
+
+  createNinja(ninja: CreateNinjaDto) {
+    const newNinja = {
+      ...ninja,
+      id: Date.now(),
+    };
+    this.ninjas.push(newNinja);
+
+    return newNinja;
+  }
+}
